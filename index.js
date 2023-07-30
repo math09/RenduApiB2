a0ee7f4aa7246405d8569a9ed3fd20b54f14fb3a
math09 <hernandezmathis@gmail.com>
2023-07-30 22:46:37 +0200

    add publicationDate

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const port = 3000;
 app.use(express.json());
 
 let books = [
-  { id: 1, title: 'Fableheaven', author: 'Brandon Mull' },
-  { id: 2, title: 'Paris 1328', author: 'Alterhis' },
+  { id: 1, title: 'Fableheaven', author: 'Brandon Mull', publicationDate: '30-07-2006' },
+  { id: 2, title: 'Paris 1328', author: 'Alterhis', publicationDate: '28-06-2023' },
 ];
 
 app.get('/api/books', (req, res) => {
   res.json(books);
 });
 
-// Fetch all book
+// Fetch book by ID
 app.get('/api/books/:id', (req, res) => {
   const bookId = parseInt(req.params.id);
   const book = books.find((book) => book.id === bookId);
@@ -24,26 +24,26 @@ app.get('/api/books/:id', (req, res) => {
 
 // Add new book
 app.post('/api/books', (req, res) => {
-    const { title, author } = req.body;
-    const newBook = { id: books.length + 1, title, author };
+    const { title, author, publicationDate } = req.body;
+    const newBook = { id: books.length + 1, title, author, publicationDate };
     books.push(newBook);
     res.status(201).json(newBook);
-  });
+});
 
-  // Update book by ID
+// Update book by ID
 app.put('/api/books/:id', (req, res) => {
     const bookId = parseInt(req.params.id);
-    const { title, author } = req.body;
+    const { title, author, publicationDate } = req.body;
     const bookIndex = books.findIndex((book) => book.id === bookId);
     if (bookIndex === -1) {
       return res.status(404).json({ error: 'Livre non trouvé' });
     }
-    const updatedBook = { id: bookId, title, author };
+    const updatedBook = { id: bookId, title, author, publicationDate };
     books[bookIndex] = updatedBook;
     res.json(updatedBook);
-  });
+});
 
-  // Delete book by ID
+// Delete book by ID
 app.delete('/api/books/:id', (req, res) => {
     const bookId = parseInt(req.params.id);
     const bookIndex = books.findIndex((book) => book.id === bookId);
@@ -52,7 +52,6 @@ app.delete('/api/books/:id', (req, res) => {
     }
     const deletedBook = books.splice(bookIndex, 1)[0];
     res.json(deletedBook);
-  });
-
-app.listen(port, () => {
 });
+
+app.listen(port, () => {});
